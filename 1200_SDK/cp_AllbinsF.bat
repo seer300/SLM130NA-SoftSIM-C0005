2c94473a7a6cb6cd59d11c84964e3fbd4f5045ba
seer300 <aasd990@126.com>
2024-11-26 09:50:48 +0800

    更新拷贝脚本,自动化备份elf文件;

--- 1200_SDK/cp_AllbinsF.bat
+++ 1200_SDK/cp_AllbinsF.bat
@@ -1,5 +1,9 @@
  copy .\Allbins\arm.img 		..\V1200FB10002R00C0005\allbins\ /B /Y
  copy .\Allbins\cp.img 		..\V1200FB10002R00C0005\allbins\ /B /Y
  copy .\Allbins\loginfo.info 	..\V1200FB10002R00C0005\allbins\ /B /Y
- copy .\xinyiNBIot_CP\.build\xinyiNBSoC.elf 	..\V1200FB10002R00C0005\allbins\ /B /Y
+@REM  复制elf
+ xcopy .\Allbins\xinyiNBIot_AP\elf ..\V1200FB10002R00C0005\elf\ap-elf /E /I /Y
+ xcopy .\Allbins\xinyiNBIot_CP\elf ..\V1200FB10002R00C0005\elf\cp-elf /E /I /Y
+ del ..\V1200FB10002R00C0005\elf\ap-elf\xinyiNBSoC.asm
+ del ..\V1200FB10002R00C0005\elf\cp-elf\xinyiNBSoC.asm
    
