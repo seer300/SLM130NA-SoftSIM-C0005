af2c11edf487747f41cf07d49fc93e727d556fec
seer300 <aasd990@126.com>
2024-12-30 13:56:53 +0800

    版本发布: T09S1231
    修复BUG：
    1、手动关闭Socket连接时，命令无响应卡死问题： 问题已修复，现均可正常关闭Socket（不受网络环境影响）。
    2、部分TCP/UDP发送命令，发送数据后，过段时间自动关闭连接问题：  已修复，如果数据发送正常，那连接将不会自动关闭（发送异常时还会关闭）。
    
    其他修复问题：
    1、修复UDP连接，如果带序列号发送失败时，连接没有关闭上报问题。
    2、修复当存在多路UDP连接时，如果某一路发送失败，没有全部连接的关闭上报问题。
    3、修复部分UDP命令（+NSOST/+NSOSTF），当带序列号发送时，没有启动超时断开机制的问题
    4、修复当服务器没有数据回复时，序列号超时机制依然断开连接问题（数据发送成功）。

--- 1200_SDK/xinyiNBIot_CP/TARGETS/xinyiNBSoC/USERAPP/inc/xy_utils_hook.h
+++ 1200_SDK/xinyiNBIot_CP/TARGETS/xinyiNBSoC/USERAPP/inc/xy_utils_hook.h
@@ -20,8 +20,8 @@
 #define BOARD_PATCH      	 "1.2.5.2"
 #define VERSION_DEV_TYPE	 "M019"
 
-#define BUILD_DATE           "1218"
-#define VERSION_INFO_NEW	 "08"
+#define BUILD_DATE           "1231"
+#define VERSION_INFO_NEW	 "09"
 
 
 /********************************** 云业务相关 **********************************/
